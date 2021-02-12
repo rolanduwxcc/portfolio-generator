3711b11f07e4a39ce87047dbc03dd92101e971a6
rolanduwxcc <rolanduwxcc@gmail.com>
2021-02-12 00:13:24 -0600

    feature sorted refactored

--- src/page-template.js
+++ src/page-template.js
@@ -13,61 +13,103 @@ const generateAbout = aboutText => {
 
 //project creation html
 const generateProjects = projectsArr => {
-  //get array of just featured projects
-  const featuredProjects = projectsArr.filter(project => {
-    if (project.feature) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  // //get array of just featured projects
+  // const featuredProjects = projectsArr.filter(project => {
+  //   if (project.feature) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
 
-  //get array fo all non-featured prjects
-  const nonFeaturedProjects = projectsArr.filter(project => {
-    if (!project.feature) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  // //get array fo all non-featured prjects
+  // const nonFeaturedProjects = projectsArr.filter(project => {
+  //   if (!project.feature) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
 
-  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-      </div>
-    `;
-  });
+  // const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+  //   return `
+  //     <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+  //       <h3 class="portfolio-item-title text-light">${name}</h3>
+  //       <h5 class="portfolio-languages">
+  //         Built With:
+  //         ${languages.join(', ')}
+  //       </h5>
+  //       <p>${description}</p>
+  //       <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+  //     </div>
+  //   `;
+  // });
 
-  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
-    return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+  // const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
+  //   return `
+  //     <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+  //         <h3 class="portfolio-item-title text-light">${name}</h3>
+  //         <h5 class="portfolio-languages">
+  //           Built With:
+  //           ${languages.join(', ')}
+  //         </h5>
+  //         <p>${description}</p>
+  //         <a href="${link}" class="btn mt-auto"><i class="fab   fa-github mr-2"></i>View Project on GitHub</a>
+  //       </div>
+  //     `;
+  //   });
+
+  // return `
+  //   <section class="my-3" id="portfolio">
+  //     <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+  //     <div class="flex-row justify-space-between">
+  //     ${featuredProjectHtmlArr.join('')}
+  //     ${nonFeaturedProjectHtmlArr.join('')}
+  //     </div>
+  //   </section>    
+  // `;
+
+  //REFACTORED USING ARRORS and 
+  return `
+  <section class="my-3" id="portfolio">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+    <div class="flex-row justify-space-between">
+    ${projectsArr
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+        <div class="col-12 mb-2 bg-dark text-light p-3">
           <h3 class="portfolio-item-title text-light">${name}</h3>
           <h5 class="portfolio-languages">
             Built With:
             ${languages.join(', ')}
           </h5>
           <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab   fa-github mr-2"></i>View Project on GitHub</a>
+          <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
         </div>
       `;
-    });
+      })
+      .join('')}
 
-  return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${featuredProjectHtmlArr.join('')}
-      ${nonFeaturedProjectHtmlArr.join('')}
-      </div>
-    </section>    
-  `;
+    ${projectsArr
+      .filter(({ feature }) => !feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
+      })
+      .join('')}
+    </div>
+  </section>
+`;
 };
 
 module.exports = templateData => {
